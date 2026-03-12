ddfdadcf24c750663a6399d9be2b0dbd71126985
haybell <ololagbele@gmail.com>
2026-03-12 10:33:19 +0100

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { notFound } from "next/navigation"
 import { blogService } from "@/lib/services"
 import { BlogPostContent } from "@/components/blog/BlogPostContent"
 
+export const dynamic = 'force-dynamic'
+
 interface BlogPostPageProps {
   params: { slug: string }
 }
@@ -14,33 +16,4 @@ export default async function BlogPostPage({ params }: BlogPostPageProps) {
   }
 
   return <BlogPostContent initialPost={post} slug={params.slug} />
-}
-
-// Generate static params for all blog posts
-export async function generateStaticParams() {
-  const allPosts = await blogService.getAllPosts()
-  return allPosts.map((post) => ({
-    slug: post.slug,
-  }))
-}
-
-// Generate metadata for each blog post
-export async function generateMetadata({ params }: BlogPostPageProps) {
-  const post = await blogService.getPostBySlug(params.slug)
-
-  if (!post) {
-    return {
-      title: "Post Not Found",
-    }
-  }
-
-  return {
-    title: `${post.title} | ÀGBÀ CINEMA Blog`,
-    description: post.excerpt,
-    openGraph: {
-      title: post.title,
-      description: post.excerpt,
-      images: [post.image],
-    },
-  }
 }
